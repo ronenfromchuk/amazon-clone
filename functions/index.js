4b44bbf54a8fcfaf943c3a09e7356421b6f0c71c
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-04-15 22:28:38 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require('stripe')('sk_test_51KjMQKJMqRtvTc9oxzTfWlk60Rn6qo1UxFhyGBhXpJkAxvBu3QHsDbdfaRlrQgRMIyRbPeyEzRvzlhnjOQnMdKaj00k7uUt80G');
+const stripe = require('stripe')('***********************************************************************************');
 
 // API
 
